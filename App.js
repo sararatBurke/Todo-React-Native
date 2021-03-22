d7e5e6dc5b0f4285e5d6f6a26747fae0f4258103
SB <sb@SARARATs-MacBook-Pro.local>
2021-03-22 18:01:30 -0400

    create expo todo app

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -15,7 +14,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginLeft: 20,
+    marginTop: 50,
   },
+
 });
