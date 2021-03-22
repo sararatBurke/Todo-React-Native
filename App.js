0f8e576e4eac788c088393b1434cf32da2890176
SB <sb@SARARATs-MacBook-Pro.local>
2021-03-22 18:11:36 -0400

    set up a structor of todo app

--- App.js
+++ App.js
@@ -2,9 +2,30 @@ import React, { useState } from 'react';
 import { Button, FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {id: '1', task: 'Do HW'},
+    {id: '2', task: 'Cooking'},
+    {id: '3', task: 'Eating'},
+    {id: '4', task: 'Do HW'},
+    {id: '5', task: 'Cooking'},
+    {id: '6', task: 'Eating'}
+  ]);
 
   return (
     <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.content}>
+        {/* Todo form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item}) => (
+              <Text>Hello</Text>
+            )}
+          />
+        </View>
+
+      </View>
       
     </View>
   );
@@ -17,6 +38,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginLeft: 20,
     marginTop: 50,
+  },
+
+  content:{
+
+  },
+
+  list:{
+
   },
 
 });
